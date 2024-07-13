efe25aacf6c0f214b62d3bd02a84a4763b6176c9
Vishal Kumar Upadhyay <46557542+vishalupadhyayrsm@users.noreply.github.com>
2024-07-13 18:46:14 +0530

    data

--- theme/forgotpassword.php
+++ theme/forgotpassword.php
@@ -11,10 +11,7 @@
     <link rel="apple-touch-icon" sizes="144x144" href="http://placehold.it/144.png/000/fff">
     <link rel="apple-touch-icon" sizes="114x114" href="http://placehold.it/114.png/000/fff">
     <link rel="apple-touch-icon" sizes="72x72" href="http://placehold.it/72.png/000/fff">
-    <!-- Standard iPhone Touch Icon-->
     <link rel="apple-touch-icon" sizes="57x57" href="http://placehold.it/57.png/000/fff">
-
-    <!-- Styles -->
     <link href="assets/css/lib/font-awesome.min.css" rel="stylesheet">
     <link href="assets/css/lib/themify-icons.css" rel="stylesheet">
     <link href="assets/css/lib/bootstrap.min.css" rel="stylesheet">
