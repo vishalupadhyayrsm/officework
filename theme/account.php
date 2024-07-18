d6e8001d672408822cc2ab6a059440af0c89ab73
Vishal Kumar Upadhyay <46557542+vishalupadhyayrsm@users.noreply.github.com>
2024-07-18 14:28:11 +0530

    data

--- theme/account.php
+++ theme/account.php
@@ -386,20 +386,12 @@ $decform = $results[0]['declarationform'];
             <script src="./js/custom.min.js"></script>
             <script src="./vendor/raphael/raphael.min.js"></script>
             <script src="./vendor/morris/morris.min.js"></script>
-
             <script src="./vendor/circle-progress/circle-progress.min.js"></script>
             <script src="./vendor/chart.js/Chart.bundle.min.js"></script>
-
             <script src="./vendor/gaugeJS/dist/gauge.min.js"></script>
-
-            <!--  flot-chart js -->
             <script src="./vendor/flot/jquery.flot.js"></script>
             <script src="./vendor/flot/jquery.flot.resize.js"></script>
-
-            <!-- Owl Carousel -->
             <script src="./vendor/owl-carousel/js/owl.carousel.min.js"></script>
-
-            <!-- Counter Up -->
             <script src="./vendor/jqvmap/js/jquery.vmap.min.js"></script>
             <script src="./vendor/jqvmap/js/jquery.vmap.usa.js"></script>
             <script src="./vendor/jquery.counterup/jquery.counterup.min.js"></script>
