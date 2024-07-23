1e2ca15f0f5752b1ab6e013330e6dd33de1f168a
Vishal Kumar Upadhyay <46557542+vishalupadhyayrsm@users.noreply.github.com>
2024-07-23 13:01:40 +0530

    data

--- theme/sidebar.php
+++ theme/sidebar.php
@@ -128,6 +128,11 @@ $usertype = $_SESSION['usertype'];
 </div>
 
 
+<?php
+// Split the $usertype string into an array of roles
+$usertypeArray = array_map('trim', explode(',', $usertype));
+?>
+
 <div class="quixnav">
     <div class="quixnav-scroll">
         <ul class="metismenu" id="menu">
@@ -135,132 +140,39 @@ $usertype = $_SESSION['usertype'];
             <li><a class="has-arrow" href="javascript:void()" aria-expanded="false"><i class="icon icon-single-04"></i><span class="nav-text" style='color:white;'>Dashboard</span></a>
                 <ul aria-expanded="false">
                     <li><a href="./user.php">Dashboard</a></li>
-                    <?php if ($usertype == 'hr' || $usertype == 'admin') {
-                    ?>
+                    <?php if (in_array('hr', $usertypeArray) || in_array('admin', $usertypeArray)) : ?>
                         <li><a href="./home.php">HR</a></li>
-                    <?php } ?>
-                    <?php
-                    if ($usertype == 'staff' || $usertype == 'intern' || $usertype == 'student') { ?>
-                    <?php }
-                    if ($usertype == 'purchase' || $usertype == 'admin') { ?>
+                    <?php endif; ?>
+                    <?php if (in_array('staff', $usertypeArray) || in_array('intern', $usertypeArray) || in_array('student', $usertypeArray)) : ?>
+                        <!-- Add specific items for these roles if needed -->
+                    <?php endif; ?>
+                    <?php if (in_array('purchase', $usertypeArray) || in_array('admin', $usertypeArray)) : ?>
                         <li><a href="./inventory.php">Amazon</a></li>
-                    <?php }
-                    if ($usertype == 'purchase') { ?>
-                    <?php } ?>
-                    <?php if ($usertype == 'account') { ?>
+                    <?php endif; ?>
+                    <?php if (in_array('account', $usertypeArray)) : ?>
                         <li><a href="./account.php">Account</a></li>
-                        <li><a href="./home.php">HR</a></li>
-                    <?php } ?>
-                    <?php if ($usertype == 'admin') { ?>
+                        <!--<li><a href="./home.php">HR</a></li>-->
+                    <?php endif; ?>
+                    <?php if (in_array('admin', $usertypeArray)) : ?>
                         <li><a href="./account.php">Account</a></li>
-                    <?php } ?>
+                    <?php endif; ?>
+                    <?php if (in_array('admin', $usertypeArray) || in_array('cash', $usertypeArray)) : ?>
+                        <li><a href="./cash.php">Cash</a></li>
+                    <?php endif; ?>
                     <li><a href="./applyleave.php">All Application</a></li>
-                    <?php if ($usertype == 'systemadmin' || $usertype == 'admin') { ?>
-                        <!--<li><a href="./systemregister.php">System Registration</a></li>-->
-                    <?php } ?>
+                    <?php if (in_array('systemadmin', $usertypeArray) || in_array('admin', $usertypeArray)) : ?>
+                        <!-- <li><a href="./systemregister.php">System Registration</a></li> -->
+                    <?php endif; ?>
                     <li><a href="/system">System</a></li>
                     <li><a href="./resign.php">Exit Formalities</a></li>
-
                 </ul>
             </li>
             <li class="nav-label">User Details</li>
             <li><a class="has-arrow" href="javascript:void()" aria-expanded="false"><i class="icon icon-app-store"></i><span class="nav-text" style='color:white;'>Profile</span></a>
                 <ul aria-expanded="false">
                     <li><a href="./userprofile.php">Profile</a></li>
-                    <!-- <li><a class="has-arrow" href="javascript:void()" aria-expanded="false">Email</a>
-                        <ul aria-expanded="false">
-                            <li><a href="./email-compose.html">Compose</a></li>
-                            <li><a href="./email-inbox.html">Inbox</a></li>
-                            <li><a href="./email-read.html">Read</a></li>
-                        </ul>
-                    </li> -->
-                    <!--<li><a href="./app-calender.html">Calendar</a></li>-->
                 </ul>
             </li>
-            <!-- <li><a class="has-arrow" href="javascript:void()" aria-expanded="false"><i class="icon icon-chart-bar-33"></i><span class="nav-text">Charts</span></a>
-                <ul aria-expanded="false">
-                    <li><a href="./chart-flot.html">Flot</a></li>
-                    <li><a href="./chart-morris.html">Morris</a></li>
-                    <li><a href="./chart-chartjs.html">Chartjs</a></li>
-                    <li><a href="./chart-chartist.html">Chartist</a></li>
-                    <li><a href="./chart-sparkline.html">Sparkline</a></li>
-                    <li><a href="./chart-peity.html">Peity</a></li>
-                </ul>
-            </li> -->
-            <!-- <li class="nav-label">Components</li>
-            <li><a class="has-arrow" href="javascript:void()" aria-expanded="false"><i class="icon icon-world-2"></i><span class="nav-text">Bootstrap</span></a>
-                <ul aria-expanded="false">
-                    <li><a href="./ui-accordion.html">Accordion</a></li>
-                    <li><a href="./ui-alert.html">Alert</a></li>
-                    <li><a href="./ui-badge.html">Badge</a></li>
-                    <li><a href="./ui-button.html">Button</a></li>
-                    <li><a href="./ui-modal.html">Modal</a></li>
-                    <li><a href="./ui-button-group.html">Button Group</a></li>
-                    <li><a href="./ui-list-group.html">List Group</a></li>
-                    <li><a href="./ui-media-object.html">Media Object</a></li>
-                    <li><a href="./ui-card.html">Cards</a></li>
-                    <li><a href="./ui-carousel.html">Carousel</a></li>
-                    <li><a href="./ui-dropdown.html">Dropdown</a></li>
-                    <li><a href="./ui-popover.html">Popover</a></li>
-                    <li><a href="./ui-progressbar.html">Progressbar</a></li>
-                    <li><a href="./ui-tab.html">Tab</a></li>
-                    <li><a href="./ui-typography.html">Typography</a></li>
-                    <li><a href="./ui-pagination.html">Pagination</a></li>
-                    <li><a href="./ui-grid.html">Grid</a></li>
-
-                </ul>
-            </li> -->
-
-            <!-- <li><a class="has-arrow" href="javascript:void()" aria-expanded="false"><i class="icon icon-plug"></i><span class="nav-text">Plugins</span></a>
-                <ul aria-expanded="false">
-                    <li><a href="./uc-select2.html">Select 2</a></li>
-                    <li><a href="./uc-nestable.html">Nestedable</a></li>
-                    <li><a href="./uc-noui-slider.html">Noui Slider</a></li>
-                    <li><a href="./uc-sweetalert.html">Sweet Alert</a></li>
-                    <li><a href="./uc-toastr.html">Toastr</a></li>
-                    <li><a href="./map-jqvmap.html">Jqv Map</a></li>
-                </ul>
-            </li> -->
-            <!-- <li><a href="widget-basic.html" aria-expanded="false"><i class="icon icon-globe-2"></i><span class="nav-text">Widget</span></a></li> -->
-
-
-            <!-- <li class="nav-label">Forms</li>
-            <li><a class="has-arrow" href="javascript:void()" aria-expanded="false"><i class="icon icon-form"></i><span class="nav-text">Forms</span></a>
-                <ul aria-expanded="false">
-                    <li><a href="./form-element.html">Form Elements</a></li>
-                    <li><a href="./form-wizard.html">Wizard</a></li>
-                    <li><a href="./form-editor-summernote.html">Summernote</a></li>
-                    <li><a href="form-pickers.html">Pickers</a></li>
-                    <li><a href="form-validation-jquery.html">Jquery Validate</a></li>
-                </ul>
-            </li> -->
-            <!-- <li class="nav-label">Table</li>
-            <li><a class="has-arrow" href="javascript:void()" aria-expanded="false"><i class="icon icon-layout-25"></i><span class="nav-text">Table</span></a>
-                <ul aria-expanded="false">
-                    <li><a href="table-bootstrap-basic.html">Bootstrap</a></li>
-                    <li><a href="table-datatable-basic.html">Datatable</a></li>
-                </ul>
-            </li> -->
-
-            <!-- <li class="nav-label">Extra</li>
-            <li><a class="has-arrow" href="javascript:void()" aria-expanded="false"><i class="icon icon-single-copy-06"></i><span class="nav-text">Pages</span></a>
-                <ul aria-expanded="false">
-                    <li><a href="./page-register.html">Register</a></li>
-                    <li><a href="./page-login.html">Login</a></li>
-                    <li><a class="has-arrow" href="javascript:void()" aria-expanded="false">Error</a>
-                        <ul aria-expanded="false">
-                            <li><a href="./page-error-400.html">Error 400</a></li>
-                            <li><a href="./page-error-403.html">Error 403</a></li>
-                            <li><a href="./page-error-404.html">Error 404</a></li>
-                            <li><a href="./page-error-500.html">Error 500</a></li>
-                            <li><a href="./page-error-503.html">Error 503</a></li>
-                        </ul>
-                    </li>
-                    <li><a href="./page-lock-screen.html">Lock Screen</a></li>
-                </ul>
-            </li> -->
         </ul>
     </div>
-
-
 </div>
